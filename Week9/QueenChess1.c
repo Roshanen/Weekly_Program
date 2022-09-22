a3519d37de788759df40b69b181578b7e4ac70ee
Roshanen <106764256+Roshanen@users.noreply.github.com>
2022-09-23 03:32:34 +0700

    Create QueenChess1.c

---
+++ Week9/QueenChess1.c
@@ -0,0 +1,45 @@
+//ตารางหมากรุกขนาด 8x8 เมื่อเราวาง Queen ทั้งหมด n ตัวไปที่ตำแหน่ง x1,y1 x2,y2 ... xn,yn
+//จงหาว่าจะมีช่องที่สามารถวางเบี้ยฝ่ายตรงข้ามได้โดยไม่ถูกกินโดย x เป็นแถว y เป็นหลัก (Level 5)
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+  char table[64];
+  memset(table, 1, 64);
+  int number;
+  int x, y;
+  int sum = 0;
+  scanf("%d", &number);
+  for (int i = 0; i < number; i++)
+  {
+    scanf("%d %d", &y, &x);
+    x--;
+    y--;
+    int pos = x + y * 8;
+    for (int j = 0; j < 8; j++)
+    {
+      table[pos] = 0;
+      table[x + j * 8] = 0;
+      table[y * 8 + j] = 0;
+      if (pos + j * 9 < 64 && x + j < 8)
+        table[(pos) + j * 9] = 0;
+      if (pos - j * 9 >= 0 && x - j >= 0)
+        table[(pos)-j * 9] = 0;
+      if (pos - j * 7 >= 0 && x + j < 8)
+        table[(pos)-j * 7] = 0;
+      if (pos + j * 7 < 64 && x - j >= 0)
+        table[(pos) + j * 7] = 0;
+    }
+  }
+  for (int i = 0; i < 8; i++)
+  {
+    for (int j = 0; j < 8; j++)
+    {
+      //printf("%d ", table[i * 8 + j]); // check table with this!
+      sum += table[i * 8 + j];
+    }
+    //printf("\n"); // next line
+  }
+  printf("%d", sum);
+  return 0;
+}
