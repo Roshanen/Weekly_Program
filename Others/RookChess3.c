f01b0d221fb5776da9f68204433897d92ad8e641
Roshanen <106764256+Roshanen@users.noreply.github.com>
2022-09-30 02:41:21 +0700

    Create RookChess3.c

---
+++ Others/RookChess3.c
@@ -0,0 +1,48 @@
+//ตารางหมากรุกขนาด 8x8 เมื่อเราวาง Rook ทั้งหมด n ตัวไปที่ตำแหน่ง x1,y1 x2,y2 ... xn,yn
+//จงหาว่าจะมีช่องที่สามารถวางเบี้ยฝ่ายตรงข้ามได้โดยไม่ถูกกินโดย x เป็นแถว y เป็นหลัก (Level 5)
+#include <stdio.h>
+int table[8][8];
+int col(int);
+int row(int);
+int main()
+{
+  int num;
+  int x, y;
+  int sum = 0;
+  scanf("%d", &num);
+  for (int i = 0; i < num; i++)
+  {
+    scanf("%d %d", &x, &y);
+    x--;
+    y--;
+    table[x][y] = 1;
+  }
+  for (int i = 0; i < 8; i++)
+  {
+    for (int j = 0; j < 8; j++)
+    {
+      if (row(i) && col(j))
+        sum++;
+    }
+  }
+  printf("%d", sum);
+  return 0;
+}
+int row(int x)
+{
+  for (int i = 0; i < 8; i++)
+  {
+    if (table[x][i] == 1)
+      return 0;
+  }
+  return 1;
+}
+int col(int y)
+{
+  for (int i = 0; i < 8; i++)
+  {
+    if (table[i][y] == 1)
+      return 0;
+  }
+  return 1;
+}
