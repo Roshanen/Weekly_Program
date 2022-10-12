abc697f12562b37bd9544a49576ff1235e22dc33
Roshanen <106764256+Roshanen@users.noreply.github.com>
2022-10-13 02:54:46 +0700

    ตึงสุดๆ

--- Others/morse.c
+++ Others/morse.c
@@ -1,72 +1,105 @@
-/* .- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--.--.- .-. ... - ..- ...- .-- -..- -.-- --..
+/* .- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..
 .---- ..--- ...-- ....- ..... -.... --... ---.. ----. ----- */
 #include <stdio.h>
-int codec[36] = {8, 46, 50, 22, 2, 34, 26, 30, 6, 44, 24, 38, 12, 10, 28, 180, 8, 18, 14, 4, 16, 32, 20, 48, 52, 5492, 76, 68, 64, 62, 94, 110, 118, 122, 124};
-char sheet[36] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-int bidec[26][2] = {{2, 4}, {16, 14}, {20, 10}, {8, 6}, {0, 2}, {4, 26}, {12, 2}, {0, 30}, {0, 6}, {14, 16}, {10, 4}, {8, 22}, {6, 0}, {4, 2}, {14, 0}, {52, 72}, {2, 4}, {4, 10}, {0, 14}, {2, 0}, {2, 12}, {2, 28}, {6, 8}, {18, 12}, {22, 8}, {96, 24}};
-void morse_to_ch(char *morse)
+int codec[36][2] = {{2, 1}, {1, 14}, {5, 10}, {1, 6}, {0, 1}, {4, 11}, {3, 4}, {0, 15}, {0, 3}, {14, 1}, {5, 2}, {2, 13}, {3, 0}, {1, 2}, {7, 0}, {6, 9}, {11, 4}, {2, 5}, {0, 7}, {1, 0}, {4, 3}, {8, 7}, {6, 1}, {9, 6}, {13, 2}, {3, 12}, {30, 1}, {28, 3}, {24, 7}, {16, 15}, {0, 31}, {1, 30}, {3, 28}, {7, 24}, {15, 16}, {31, 0}};
+char ch[36] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+int dec = 0;
+int dot = 0;
+void codectoch(int dec, int dot)
 {
-    while (1)
+    for (int i = 0; i < 36; i++)
     {
-        char code[5];
-        int dec = 0;
-        int dot = 0;
-        int i = 0;
-        for (i = 0; i < 6; i++)
+        if (dec == codec[i][0] && dot == codec[i][1])
         {
-            scanf("%c", &code[i]);
-            if (code[i] == ' ')
-                break;
-            else if (code[i] == '-')
-                dec++;
-            else if (code[i] == '.')
-                dot++;
-            dec *= 2;
-            dot *= 2;
-        }
-        for (int i = 0; i < 37; i++)
-        {
-            if (dec * 2 + dot == codec[i])
-                printf("%c,", sheet[i]);
-        }
-        if (code[i] == ' ')
+            printf("%c", ch[i]);
             break;
+        }
     }
 }
-void ch_to_morse(char *ch)
+void morse_codec(char *word)
 {
-    char code[6];
-    int i = 0;
-    while (1)
+    int others = 0;
+    int a = 0;
+    while (others < 2)
     {
-        for (i = 0; i < 6; i++)
-            scanf("%c", &code[i]);
+        if (word[a] != '-' && word[a] != '.')
+        {
+            dec = 0;
+            dot = 0;
+            for (int i = 1; i < 6; i++)
+            {
+                if (word[a - i] != '-' && word[a - i] != '.')
+                    break;
+                else if (word[a - i] == '-')
+                {
+                    dec++;
+                    others = 0;
+                }
+                else if (word[a - i] == '.')
+                {
+                    dot++;
+                    others = 0;
+                }
+                dec *= 2;
+                dot *= 2;
+            }
+            // printf("{%d,%d},", dec / 2, dot / 2);
+            codectoch(dec/2, dot/2);
+            others++;
+        }
+        a++;
+    }
+}
+void codectomorse(int dec, int dot)
+{
+    while (dec > 0 || dot > 0)
+    {
+        if (dec % 2 != 0)
+        {
+            printf("-");
+            dec--;
+        }
+        else if (dot % 2 != 0)
+        {
+            printf(".");
+            dot--;
+        }
+        dec /= 2;
+        dot /= 2;
+    }
+}
+void ch_codec(char *word)
+{
+    int others = 0;
+    int a = 0;
+    while (others < 2)
+    {
+        if ((word[a] >= 'A' && word[a] <= 'Z') || (word[a] >= '0' && word[a] <= '9'))
+        {
+            for (int i = 0; i < 36; i++)
+            {
+                if (ch[i] == word[a])
+                {
+                    dec = codec[i][0];
+                    dot = codec[i][1];
+                    codectomorse(dec, dot);
+                    printf("\\");
+                    break;
+                }
+            }
+        }
+        else
+            others++;
+        a++;
     }
 }
 int main()
 {
-    int blank = 0;
-    int i = 0;
-    char code[6];
-    while (code[i] != ' ')
-    {
-        blank = 0;
-        int dec = 0;
-        int dot = 0;
-        for (i = 0; i < 6; i++)
-        {
-            scanf("%c", &code[i]);
-            if (code[i] == '/')
-                break;
-            else if (code[i] == '-')
-                dec++;
-            else if (code[i] == '.')
-                dot++;
-            dec *= 2;
-            dot *= 2;
-        }
-        // printf("%d,", dec * 2 + dot);
-        printf("{%d,%d},", dec, dot);
-    }
+    char word[300];
+    scanf("%[^\n]", word);
+    if (word[0] != '.' && word[0] != '-')
+        ch_codec(word);
+    else
+        morse_codec(word);
     return 0;
 }
